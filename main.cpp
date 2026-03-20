234746e23819534d710fa43e0fac56c1abfc88e8
Andrea Pollini <prof.andrea.pollini@gmail.com>
2026-03-20 09:28:51 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "operazioni.h"
 
-// TODO: redkjhckhjkfhdskjhfdskjhskdfj è un po' confusionario, magari faccio una classe che si chiama Operazioni e metto dentro tutte le funzioni, così è più ordinato
+// TODO: dfgdfgd è un po' confusionario, magari faccio una classe che si chiama Operazioni e metto dentro tutte le funzioni, così è più ordinato
 using namespace std;
 //345834953098
 
