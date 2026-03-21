231982cc734e16be331195965c1a87e15a5a404b
Maria Coffani <coffani_maria@yahoo.it>
2026-03-21 17:01:25 +0100

    Codice ripulito, eliminato commenti

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
 #include "operazioni.h"
 
-// TODO: redkjhckhjkfhdskjhfdskjhskdfj è un po' confusionario, magari faccio una classe che si chiama Operazioni e metto dentro tutte le funzioni, così è più ordinato
+
 using namespace std;
-//345834953098
-
-// prova 42424242
-
-//pluto
 
 int main() {
     cout << "La somma di due numeri" << endl;
     cout << calcolaSomma(5,2);
 
     cout << "calcolo effettuato" << endl;
-    cout << "provo a fare add" <<endl;
-    cout << "PROVA CODICE RIGA 18";
-
     return 0;
 }
